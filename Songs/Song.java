e836a1c9352c282db2b0cf0f1fbe96629892c0d4
dcs0spb <s.p.bradley@durham.ac.uk>
2017-10-19 13:06:46 +0100

    lecture 4

--- Songs/Song.java
+++ Songs/Song.java
@@ -2,4 +2,21 @@
 public class Song
 {
     private int year;
+    private String genre;
+    private String name;
+    private int tempo; // bpm
+    
+    public Song(String songName){
+        name = songName;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setGenre(String g){
+        genre = g;
+    }
+      
+      
 }
