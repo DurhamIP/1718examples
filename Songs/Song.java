c4263d07570032836565788f63b08357c7ec1f30
dcs0spb <s.p.bradley@durham.ac.uk>
2017-10-24 13:25:49 +0100

    print method with if statement

--- Songs/Song.java
+++ Songs/Song.java
@@ -5,18 +5,26 @@ public class Song
     private String genre;
     private String name;
     private int tempo; // bpm
-    
+
     public Song(String songName){
         name = songName;
     }
-    
+
     public String getName(){
         return name;
     }
-    
+
     public void setGenre(String g){
         genre = g;
     }
-      
-      
+
+    public void print(){
+        if(genre == null){
+            System.out.println("Title: " + name);      
+        }
+        else{
+            System.out.println("Title: " + name + " genre: " + genre);   
+        }
+    }
+
 }
