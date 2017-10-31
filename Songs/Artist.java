6760e680977a9195d34af7e6a91506ae6713c089
dcs0spb <s.p.bradley@durham.ac.uk>
2017-10-31 16:21:44 +0000

    chapter 3: added a new class Artist which is used by Song

---
+++ Songs/Artist.java
@@ -0,0 +1,17 @@
+
+
+public class Artist
+{
+    private String name;
+    private java.util.Date dob;
+    private String instrument;
+    private String recordLabel;
+    
+    public Artist(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+}
--- Songs/Song.java
+++ Songs/Song.java
@@ -5,13 +5,24 @@ public class Song
     private String genre;
     private String name;
     private int tempo; // bpm
+    private Artist artist;
 
-    public Song(String songName){
+    public Song(String songName, Artist a){
         name = songName;
+        artist = a;
+    }
+
+    public Song(String songName, String artistName){
+        name = songName;
+        artist = new Artist(artistName);
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getArtistName(){
+        return artist.getName();
     }
 
     public void setGenre(String g){
@@ -25,6 +36,7 @@ public class Song
         else{
             System.out.println("Title: " + name + " genre: " + genre);   
         }
+        System.out.println("Artist:" + artist.getName());
     }
 
 }
