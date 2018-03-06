d3970a936d9c37369f254c279f92404554152612
dcs0spb <s.p.bradley@durham.ac.uk>
2018-03-06 11:01:11 +0000

    Basic GUI functionality

--- Songs/Song.java
+++ Songs/Song.java
@@ -1,5 +1,14 @@
 import java.io.*;
 import java.util.*;
+import javafx.stage.*;
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.embed.swing.*;
 
 public class Song implements Comparable<Song>
 {
@@ -152,6 +161,33 @@ public class Song implements Comparable<Song>
             System.out.println("Bad year: " + year);
             return null;
         }
+    }
+
+    public void initialiseGUI1(){ 
+        Stage stage = new Stage();
+        stage.setTitle("Hello World"); 
+        stage.show();
+    }
+
+    public void initialiseGUI2(){
+        Stage stage = new Stage(); 
+        stage.setTitle("Hello World");
+        Button btn = new Button(); 
+        btn.setText("Say 'hello world'");
+        btn.setOnAction(new EventHandler<ActionEvent>() { @Override
+                public void handle(ActionEvent event){ System.out.println("Hello World");
+                }
+            });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn); stage.setScene(new Scene(root, 300, 250));
+        stage.show();
+    }
+
+    public void launchFX(){ 
+        new JFXPanel();
+        Platform.setImplicitExit(false); 
+        Platform.runLater(() -> initialiseGUI2());
     }
 
 }
